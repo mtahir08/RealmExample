9b5dd5c654e34b89b832987b094ebfc984565f59
Muhammad Tahir <mtahir.ps@gmail.com>
2018-11-21 20:41:11 +0500

    TodoListComponent

--- components/TodoListComponent.js
+++ components/TodoListComponent.js
@@ -11,18 +11,57 @@ import {
 import {
   updateTodoList,
   deleteTodoList,
-//   queryAllTodoLists
+  queryAllTodoLists
 } from '../database/allSchemas';
 
-// import realm from '../database/allSchemas';
+import realm from '../database/allSchemas';
+import HeaderComponent from './HeaderComponent';
+import PopupDialogComponent from './PopupDialogComponent';
 
 class TodoListComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      todoLists: []
+    };
   }
+
+  reloadData = () => {
+    queryAllTodoLists()
+      .then(todoLists => {
+        this.setState({ todoLists });
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ todoLists: [] });
+      });
+  };
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <HeaderComponent
+          title={'Todo List'}
+          hasAddButton={true}
+          showAddTodoList={() =>
+            this.refs.popupDialogComponent.showDialogComponentForAdd()
+          }
+        />
+        <FlatList
+          style={styles.flatList}
+          data={this.state.todoLists}
+          renderItem={(item, index) => {
+            <FlatListItem
+              {...item}
+              itemIndex={index}
+              popupDialogComponent={this.refs.popupDialogComponent}
+              onPress={() => alert('You pressed')}
+              keyExtractor={item => item.id}
+            />;
+          }}
+        />
+        <PopupDialogComponent ref={'popupDialogComponent'} />
+      </View>
+    );
   }
 }
 
